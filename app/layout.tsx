6dda10c057a4b4216fe77bfb98d1d1a3e9014490
Aria Taylor <ari@aricodes.net>
2025-08-13 10:44:21 -0700

    Add plausible analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { ColorSchemeScript, MantineProvider, mantineHtmlProps } from '@mantine/core';
 import '@mantine/core/styles.css';
 import type { Metadata } from 'next';
+import Script from 'next/script';
 
 import theme from './theme';
 
@@ -19,6 +20,11 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <Script
+          defer
+          data-domain="asabovesound.com"
+          src="https://plausible.aricodes.net/js/script.outbound-links.js"
+        />
         <ColorSchemeScript />
       </head>
       <body>
