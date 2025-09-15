48dff667fd98fe6a10a30f699c933744a04354e6
Aria Taylor <ari@aricodes.net>
2025-09-15 13:14:32 -0700

    Don't crash remote environment on failed fetch

--- components/Shows/index.tsx
+++ components/Shows/index.tsx
@@ -14,26 +14,30 @@ const startTime = (show: calendar_v3.Schema$Event) =>
 export default async function Shows() {
   'use cache';
   // Fetch all events we can see
-  const events = await calendar.events.list({ calendarId, maxResults: 1500 });
-  // const events = { data: { items } };
+  try {
+    const events = await calendar.events.list({ calendarId, maxResults: 1500 });
 
-  // If we have none, show some text
-  if (!events.data.items) {
+    // If we have none, show some text
+    if (!events.data.items) {
+      return <Text>Unable to pull show information, check back later!</Text>;
+    }
+
+    // Otherwise, sort into upcoming and past
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    const daysUntil = (date: Date) =>
+      Math.ceil((date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+
+    const shows = events.data.items.toSorted(
+      (a, b) => daysUntil(startTime(a)) - daysUntil(startTime(b)),
+    );
+
+    const upcoming = shows.filter(show => daysUntil(startTime(show)) >= 0);
+    const past = shows.filter(show => daysUntil(startTime(show)) < 0);
+
+    return <Table {...{ upcoming, past }} />;
+  } catch (e) {
+    console.error('CALENDAR', e);
     return <Text>Unable to pull show information, check back later!</Text>;
   }
-
-  // Otherwise, sort into upcoming and past
-  const today = new Date();
-  today.setHours(0, 0, 0, 0);
-  const daysUntil = (date: Date) =>
-    Math.ceil((date.getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
-
-  const shows = events.data.items.toSorted(
-    (a, b) => daysUntil(startTime(a)) - daysUntil(startTime(b)),
-  );
-
-  const upcoming = shows.filter(show => daysUntil(startTime(show)) >= 0);
-  const past = shows.filter(show => daysUntil(startTime(show)) < 0);
-
-  return <Table {...{ upcoming, past }} />;
 }
