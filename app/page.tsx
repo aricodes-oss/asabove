73b5e9004a3a0ac40db5b2fff459914630e2e6dd
Aria Taylor <ari@aricodes.net>
2026-01-12 14:07:12 -0800

    Add countdown timer to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,50 +3,62 @@
 import { getCountdowns, getShows } from '@/api';
 import Calendar from '@/components/Calendar';
 import Contact from '@/components/Contact';
+import Countdown from '@/components/Countdown';
 import Frame from '@/components/Frame';
 import Header from '@/components/Header';
 import TV from '@/components/TV';
 import { socials } from '@/constants';
-import { Container, Flex } from '@mantine/core';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+import { Box, Container, Flex } from '@mantine/core';
 import Image from 'next/image';
 
 import classes from './page.module.scss';
 
 const instagram = socials.find(link => link.text === 'Instagram');
-
 export default async function Home() {
   const shows = await getShows();
   const countdowns = await getCountdowns();
+  let countTo = null;
 
-  console.log(countdowns);
+  if (countdowns?.upcoming?.length) {
+    countTo = countdowns.upcoming[0];
+  }
 
   return (
     <>
       <Header />
 
-      <Container fluid px={{ base: '8px', md: '16px' }} className={classes.header}>
+      <Container fluid px={{ base: '8px', lg: '16px' }} className={classes.header}>
         <Flex
-          direction={{ base: 'column', md: 'row' }}
-          justify={{ base: 'flex-start', md: 'space-between' }}
-          align={{ base: 'center', md: 'flex-start' }}
+          direction={{ base: 'column', lg: 'row' }}
+          justify={{ base: 'flex-start', lg: 'space-between' }}
+          align={{ base: 'center', lg: 'flex-start' }}
+          gap="md"
         >
-          <Frame href={instagram!.href} className={classes.frame}>
-            <Image
-              src="/band-lowres2.jpg"
-              alt="Photo of the band"
-              fill={true}
-              className={classes.img}
-            />
-          </Frame>
-          <Flex direction={{ base: 'column', md: 'row' }} className={classes.right}>
+          <Flex direction="column" style={{ flexGrow: 1, flexShrink: 0 }}>
+            <Frame href={instagram!.href} className={classes.frame}>
+              <Image
+                src="/band-lowres2.jpg"
+                alt="Photo of the band"
+                fill={true}
+                className={classes.img}
+              />
+            </Frame>
+            <Box visibleFrom="sm" px={16}>
+              {countTo && (
+                <Countdown to={new Date(countTo.start!.date as string)} title={countTo.summary} />
+              )}
+            </Box>
+          </Flex>
+          <Flex direction={{ base: 'column', lg: 'row' }} className={classes.right}>
             <Contact />
             <Calendar {...shows}></Calendar>
           </Flex>
         </Flex>
 
         <Flex
-          direction={{ base: 'column', md: 'row' }}
-          align={{ base: 'center', md: 'flex-start' }}
+          direction={{ base: 'column', lg: 'row' }}
+          align={{ base: 'center', lg: 'flex-start' }}
         >
           <div style={{ flexGrow: 1 }}>
             <TV />
---
+++ components/Countdown.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import FlipClockCountdown from '@leenguyen/react-flip-clock-countdown';
+import '@leenguyen/react-flip-clock-countdown/dist/index.css';
+
+interface CountdownProps {
+  to: Date;
+  title?: string;
+}
+
+export default function Countdown({ to, title, ...props }: CountdownProps) {
+  return (
+    <>
+      {title && <span style={{ fontSize: '48px' }}>{title}</span>}
+      <FlipClockCountdown to={to} {...props} />
+    </>
+  );
+}
