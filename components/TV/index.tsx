2becee5df6a9d011fca191d6d3c0d50f142fef2d
Aria Taylor <ari@aricodes.net>
2026-01-20 13:02:03 -0800

    Switch video to official upside down music video

--- components/TV/index.tsx
+++ components/TV/index.tsx
@@ -9,7 +9,7 @@ export default function TV() {
   return (
     <div className={classes.root}>
       <iframe
-        src="https://www.youtube.com/embed/7Mf1tcQAbGI?si=olX4lTz9VBtVVS73"
+        src="https://www.youtube.com/embed/tmHkkbK-4U8"
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
